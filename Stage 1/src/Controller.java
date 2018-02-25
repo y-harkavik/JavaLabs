420aa396d98be1c512d2aab912887cadc7f55990
unknown <bugatti1701@gmail.com>
2018-02-25 14:21:28 +0300

    add items

--- Stage 1/src/Controller.java
+++ Stage 1/src/Controller.java
@@ -3,52 +3,49 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 
 
 import java.io.File;
 
 
 public class Controller {
-    @FXML private TableView<Item> tableView;
-    @FXML private TableColumn previewColumn;
-    @FXML private TableColumn nameColumn;
-    @FXML private TableColumn typeColumn;
-    @FXML private TableColumn sizeColumn;
-    @FXML private TableColumn dateColumn;
+    @FXML
+    private TableView<Item> tableView;
+    /*@FXML
+    private TableColumn previewColumn;
+    @FXML
+    private TableColumn nameColumn;
+    @FXML
+    private TableColumn typeColumn;
+    @FXML
+    private TableColumn sizeColumn;
+    @FXML
+    private TableColumn dateColumn;*/
 
     private Logic logic = new Logic();
 
     @FXML
     private void documentAction(ActionEvent event) {
-        //previewColumn.setCellFactory(new PropertyValueFactory<>("pre"));
-
-        File file = logic.getFile(logic.DOCUMENTS);
-        Item item = new Item(file,logic.DOCUMENTS);
-        ObservableList<Item> data= FXCollections.observableArrayList(
-                item
-        );
-        tableView.getItems().addAll(data);
+        tableView.getItems().addAll(logic.getItems(Logic.DOCUMENTS));
     }
 
     @FXML
     private void imagesAction(ActionEvent event) {
-        File file = logic.getFile(logic.IMAGES);
+        // File file = logic.getFile(logic.IMAGES);
 
     }
 
     @FXML
     private void videoAction(ActionEvent event) {
-        File file = logic.getFile(logic.VIDEO);
+        //File file = logic.getFile(logic.VIDEO);
 
     }
 
     @FXML
     private void audioAction(ActionEvent event) {
-        File file = logic.getFile(logic.AUDIO);
+        // File file = logic.getFile(logic.AUDIO);
 
     }
 
--- Stage 1/src/Logic.java
+++ Stage 1/src/Logic.java
@@ -1,45 +1,90 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
 import java.lang.String;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class Logic {
+    private Data data = new Data();
 
     public static final String DOCUMENTS = "DOCUMENTS";
     public static final String IMAGES = "IMAGES";
     public static final String VIDEO = "VIDEO";
     public static final String AUDIO = "AUDIO";
 
-    private static final String[] DOCUMENTS_EXTENSIONS = {"*.docx","*.doc","*.ppt",".pptx","*.xlsx"};
-    private static final String[] IMAGES_EXTENSIONS = {"*.png","*.jpg","*.jpeg","*.bmp"};
-    private static final String[] VIDEO_EXTENSIONS = {"*.mkv","*.avi","*.mp4"};
-    private static final String[] AUDIO_EXTENSIONS = {"*.mp3","*.wav"};
+    private static final String[] DOCUMENTS_EXTENSIONS = {"*.docx", "*.doc", "*.ppt", ".pptx", "*.xlsx"};
+    private static final String[] IMAGES_EXTENSIONS = {"*.png", "*.jpg", "*.jpeg", "*.bmp"};
+    private static final String[] VIDEO_EXTENSIONS = {"*.mkv", "*.avi", "*.mp4"};
+    private static final String[] AUDIO_EXTENSIONS = {"*.mp3", "*.wav"};
 
-    File getFile(String neededFiles) {
-        FileChooser fileChooser= new FileChooser();
-        setFilter(fileChooser,neededFiles);
-        return fileChooser.showOpenDialog(new Stage());
+    class Data {
+        private HashMap<String, ArrayList<Item>> listOfItem = new HashMap<String, ArrayList<Item>>();
+
+        Data() {
+            /*listOfItem.put(DOCUMENTS, FXCollections.observableArrayList());
+            listOfItem.put(IMAGES, FXCollections.observableArrayList());
+            listOfItem.put(VIDEO, FXCollections.observableArrayList());
+            listOfItem.put(AUDIO, FXCollections.observableArrayList());*/
+            listOfItem.put(DOCUMENTS, new ArrayList<Item>());
+            listOfItem.put(IMAGES, new ArrayList<Item>());
+            listOfItem.put(VIDEO, new ArrayList<Item>());
+            listOfItem.put(AUDIO, new ArrayList<Item>());
+        }
+
+        List<Item> getItemsList(String key) {
+            return listOfItem.get(key);
+        }
+
+        void addItemInList(Item item) {
+            listOfItem.get(item.getItemType()).add(item);
+        }
     }
 
-    private void setFilter(FileChooser fileChooser,String neededFiles) {
-        switch(neededFiles) {
+    List<Item> addFiles(String typeOfItem) {
+        getInformation(getFiles(typeOfItem),typeOfItem);
+        return getItems(typeOfItem);
+    }
+
+    List<Item> getItems(String type) {
+        return data.getItemsList(type);
+    }
+
+    void getInformation(List<File> files,String typeOfItem) {
+        for (File file : files) {
+            data.addItemInList(new Item(file,typeOfItem));
+        }
+    }
+
+    List<File> getFiles(String neededFiles) {
+        FileChooser fileChooser = new FileChooser();
+        setFilter(fileChooser, neededFiles);
+        return fileChooser.showOpenMultipleDialog(new Stage());
+    }
+
+    private void setFilter(FileChooser fileChooser, String neededFiles) {
+        switch (neededFiles) {
             case DOCUMENTS:
                 fileChooser.getExtensionFilters().addAll(
-                    new FileChooser.ExtensionFilter("Documents",DOCUMENTS_EXTENSIONS)
-            );
+                        new FileChooser.ExtensionFilter("Documents", DOCUMENTS_EXTENSIONS)
+                );
             case IMAGES:
                 fileChooser.getExtensionFilters().addAll(
-                    new FileChooser.ExtensionFilter("Images",IMAGES_EXTENSIONS)
-            );
+                        new FileChooser.ExtensionFilter("Images", IMAGES_EXTENSIONS)
+                );
             case VIDEO:
                 fileChooser.getExtensionFilters().addAll(
-                    new FileChooser.ExtensionFilter("Video",VIDEO_EXTENSIONS)
-            );
+                        new FileChooser.ExtensionFilter("Video", VIDEO_EXTENSIONS)
+                );
             case AUDIO:
                 fileChooser.getExtensionFilters().addAll(
-                    new FileChooser.ExtensionFilter("Audio",AUDIO_EXTENSIONS)
-            );
+                        new FileChooser.ExtensionFilter("Audio", AUDIO_EXTENSIONS)
+                );
         }
     }
 }
