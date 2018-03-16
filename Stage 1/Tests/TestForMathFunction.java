4b9c8b3888029698f1f040b90a1b0359674b66b0
unknown <bugatti1701@gmail.com>
2018-03-16 14:39:20 +0300

    add funcTest2 for MathFunc

--- Stage 1/Tests/TestForMathFunction.java
+++ Stage 1/Tests/TestForMathFunction.java
@@ -1,12 +1,26 @@
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestForMathFunction {
+    private static MathFunction mathFunction;
+    @BeforeClass
+    public static void initMathFunc() {
+        mathFunction = new MathFunction();
+    }
     @Test
-    public void functionTest() {
+    public void unctionTest1() {
         long x = 1, y = 1;
-        MathFunction mathFunction = new MathFunction();
         double expected = 1.65119;
+        double actual = mathFunction.start(x,y);
+
+        Assert.assertEquals("Тест не прошел, т.к. не равны значения.",expected, actual,0.00001);
+    }
+
+    @Test
+    public void functionTest2() {
+        long x = 2, y = 0;
+        double expected = 2.32559;
         double actual = mathFunction.start(x,y);
 
         Assert.assertEquals("Тест не прошел, т.к. не равны значения.",expected, actual,0.00001);
