4570d6908eb5e42ff51f4ddd03ff297b6e16f571
unknown <bugatti1701@gmail.com>
2018-04-26 12:51:13 +0300

    added users type classes

---
+++ Stage 3. Archive/src/person/Admin.java
@@ -0,0 +1,4 @@
+package person;
+
+public class Admin {
+}
---
+++ Stage 3. Archive/src/person/Person.java
@@ -0,0 +1,40 @@
+package person;
+
+import java.io.Serializable;
+
+public abstract class Person implements Serializable {
+    protected byte[] salt;
+    protected String passwordAndSaltHash;
+    protected String accountLogin;
+    protected Laws userLaws;
+
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+
+    public Person(String password, String accountLogin, byte[] salt, Laws userLaws) {
+        passwordAndSaltHash = password;
+        this.salt = salt;
+        this.accountLogin = accountLogin;
+        this.userLaws = userLaws;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public String getAccountLogin() {
+        return accountLogin;
+    }
+
+    public String getPasswordAndSaltHash() {
+        return passwordAndSaltHash;
+    }
+
+    public void setUserLaws() {
+
+    }
+
+    public Laws getUserLaws() {
+        return userLaws;
+    }
+}
